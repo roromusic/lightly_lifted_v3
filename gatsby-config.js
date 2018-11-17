fff8a5576ef675c5cf033159e4d26227172b7156
roromusic <roromusic@ucla.edu>
2018-11-16 22:29:36 -0800

    yay

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Lightly Lifted',
   },
   plugins: [
     `gatsby-plugin-emotion`,
@@ -23,7 +23,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
